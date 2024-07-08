2237c28d58b6c3afa2817c1361574dd1ee0900cf
Nik Damis <nik.damis@gmail.com>
2024-07-08 14:14:51 +0200

    working script for products, products.json

--- js/script.js
+++ js/script.js
@@ -1,4 +1,69 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('JavaScript loaded');
+  const navbar = document.querySelector('.navbar');
+
+  const handleScroll = () => {
+    if (window.scrollY > 5) {
+      navbar.classList.add('navbar-transparent');
+      navbar.classList.remove('bg-dark');
+    } else {
+      navbar.classList.remove('navbar-transparent');
+      navbar.classList.add('bg-dark');
+    }
+  };
+
+  handleScroll();
+  window.addEventListener('scroll', handleScroll);
+
+  fetch('data/products.json')
+    .then(response => response.json())
+    .then(data => {
+      displayProducts('coffee', data);
+    })
+    .catch(error => console.error('Error fetching product data:', error));
+
+  document.querySelectorAll('.dropdown-item').forEach(item => {
+    item.addEventListener('click', event => {
+      const category = event.target.getAttribute('onclick').split("'")[1];
+      fetch('data/products.json')
+        .then(response => response.json())
+        .then(data => {
+          displayProducts(category, data);
+        })
+        .catch(error => console.error('Error fetching product data:', error));
+    });
   });
+});
+
+function displayProducts(category, data) {
+  const productContainer = document.getElementById('product-container');
+  productContainer.innerHTML = '';
   
+  // Iterate over the subcategories
+  for (let subcategory in data[category]) {
+    data[category][subcategory].forEach(product => {
+      const productCard = `
+        <div class="col-md-4 product" data-category="${category}">
+          <div class="card mb-4">
+            <img src="${product.image}" class="card-img-top" alt="${product.name}">
+            <div class="card-body">
+              <h5 class="card-title">${product.name}</h5>
+              <p class="card-text">${product.description}</p>
+            </div>
+          </div>
+        </div>
+      `;
+      productContainer.insertAdjacentHTML('beforeend', productCard);
+    });
+  }
+}
+
+function filterProducts(category) {
+  const products = document.querySelectorAll('.product');
+  products.forEach(product => {
+    product.style.display = product.getAttribute('data-category') === category ? 'block' : 'none';
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  filterProducts('coffee');
+});
